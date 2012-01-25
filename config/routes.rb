6492f554ceffc8fc812ca64ccd9ee063eead4441
Ben Nguyen <hungryhippo138@gmail.com>
2012-01-24 16:02:15 -0800

    commit with routes change

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rubyroute::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'blog#index'
+   root :to => 'passthru#index'
 
   # See how all your routes lay out with "rake routes"
 
